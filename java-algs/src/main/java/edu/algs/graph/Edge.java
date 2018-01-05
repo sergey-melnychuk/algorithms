fb2f385f5b0043cb0bca6fe649c46cc1c36e3cfc
sergey-melnychuk <serg.meln@gmail.com>
2018-01-06 00:19:31 +0100

    Add graph representation and a floyd, kruskal and bfs implementations

---
+++ java-algs/src/main/java/edu/algs/graph/Edge.java
@@ -0,0 +1,55 @@
+package edu.algs.graph;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+class Edge {
+    static final int W = 1; // default weight of the edge (used in un-weighted graph)
+
+    final int source;
+    final int target;
+    final int weight;
+
+    public Edge(int source, int target, int weight) {
+        this.source = source;
+        this.target = target;
+        this.weight = weight;
+    }
+
+    public Edge(int source, int target) {
+        this(source, target, W);
+    }
+
+    public Edge reverse() {
+        return new Edge(target, source, weight);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Edge edge = (Edge) o;
+        return source == edge.source &&
+                target == edge.target &&
+                weight == edge.weight;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(source, target, weight);
+    }
+
+    @Override
+    public String toString() {
+        return "Edge{" + source + "->" + target + ", w=" + weight + '}';
+    }
+
+    static Comparator<Edge> ASC = (Edge lhs, Edge rhs) -> {
+        if (lhs.weight != rhs.weight) return (int) (lhs.weight - rhs.weight);
+        if (lhs.source != rhs.source) return lhs.source - rhs.source;
+        if (lhs.target != rhs.target) return lhs.target - rhs.target;
+        return 0;
+    };
+
+    static Comparator<Edge> DESC = (Edge lhs, Edge rhs) -> ASC.compare(rhs, lhs);
+}
---
+++ java-algs/src/main/java/edu/algs/graph/Graph.java
@@ -0,0 +1,216 @@
+package edu.algs.graph;
+
+import edu.algs.dset.DSet;
+
+import java.util.*;
+
+public class Graph {
+    private final int N;
+    private final List<List<Edge>> edges;
+
+    Graph(final int nrOfNodes) {
+        if (nrOfNodes <= 0) {
+            throw new IllegalStateException("Number of nodes must be positive");
+        }
+        this.N = nrOfNodes;
+        this.edges = new ArrayList<>(N);
+        for (int i=0; i<N; i++) {
+            this.edges.add(new ArrayList<>());
+        }
+    }
+
+
+
+    public int[][] getMatrix() { return getMatrix(false); }
+
+    public int[][] getMatrix(boolean directed) {
+        int result[][] = new int[N][N];
+        for (int i=0; i<N; i++) {
+            for (Edge e : edges.get(i)) {
+                result[e.source][e.target] = e.weight;
+                if (!directed) {
+                    result[e.target][e.source] = e.weight;
+                }
+            }
+        }
+        return result;
+    }
+
+
+
+    public void edge(Edge e) { edges.get(e.source).add(e); }
+
+    public void edge(int u, int v) { edge(u, v, Edge.W); }
+
+    public void edge(int u, int v, int w) { edge(u, v, w, false); }
+
+    public void edge(int u, int v, int w, boolean symmetric) {
+        edges.get(u).add(new Edge(u, v, w));
+        if (symmetric) {
+            edges.get(v).add(new Edge(v, u, w));
+        }
+    }
+
+
+
+    public List<Edge> adj(int u) { return adj(u, false, false); }
+
+    public List<Edge> adj(int u, boolean sorted, boolean reverse) {
+        if (!sorted) {
+            return edges.get(u);
+        }  else {
+            List<Edge> result = new ArrayList<>(edges.get(u));
+            Comparator<Edge> cmp = Edge.ASC;
+            if (reverse) {
+                cmp = Edge.DESC;
+            }
+            result.sort(cmp);
+            return result;
+        }
+    }
+
+
+
+    public List<Edge> getEdges() { return getEdges(false); }
+
+    public List<Edge> getEdges(boolean sorted) {
+        List<Edge> result = new ArrayList<>();
+        for (int i=0; i<N; i++) {
+            result.addAll(edges.get(i));
+        }
+        if (sorted) {
+            result.sort(Edge.ASC);
+        }
+        return result;
+    }
+
+
+
+    public long weight() {
+        long sum = 0L;
+        for (int i=0; i<N; i++)
+            for (Edge e : adj(i))
+                sum += e.weight;
+        return sum;
+    }
+
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Size size = size();
+        sb.append(size.nodes);
+        sb.append(" ");
+        sb.append(size.edges);
+        sb.append("\n");
+        for (int i=0; i<size.nodes; i++) {
+            for (Edge e : adj(i)) {
+                sb.append(e.source);
+                sb.append(" ");
+                sb.append(e.target);
+                sb.append(" ");
+                sb.append(e.weight);
+                sb.append("\n");
+            }
+        }
+        return sb.toString();
+    }
+
+    public Size size() {
+        int count = 0;
+        for (int i = 0; i < N; i++) {
+            count += adj(i).size();
+        }
+        return new Size(N, count);
+    }
+
+    public static class Size {
+        final int nodes;
+        final int edges;
+
+        public Size(int nodes, int edges) {
+            this.nodes = nodes;
+            this.edges = edges;
+        }
+    }
+
+
+
+    public long[] bfs(int source) {
+        for (Edge e : getEdges()) {
+            if (e.weight != 1) {
+                throw new UnsupportedOperationException("Can't run BFS on weighted graph");
+            }
+        }
+        long dist[] = new long[N];
+        for (int i=0; i<N; i++) {
+            dist[i] = -1;
+        }
+        dist[source] = 0;
+        for (Edge e : adj(source)) {
+            dist[e.target] = e.weight;
+        }
+        BitSet seen = new BitSet(N);
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(source);
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            long cost = dist[node];
+            seen.set(node);
+            for (Edge e : adj(node)) {
+                long d = dist[e.target];
+                if (d < 0 || d > cost + e.weight) {
+                    dist[e.target] = cost + e.weight;
+                }
+                if (!seen.get(e.target)) {
+                    queue.offer(e.target);
+                }
+            }
+        }
+        return dist;
+    }
+
+    public long[][] floyd() {
+        final long INF = Long.MAX_VALUE/2 -1;
+        long dist[][] = new long [N][N];
+
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<N; j++) {
+                dist[i][j] = INF;
+            }
+            dist[i][i] = 0;
+        }
+
+        for (int i=0; i<N; i++) {
+            for (Edge e : adj(i)) {
+                dist[i][e.target] = e.weight;
+            }
+        }
+
+        for (int k=0; k<N; k++) {
+            for (int i=0; i<N; i++) {
+                for (int j=0; j<N; j++) {
+                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+        return dist;
+    }
+
+    public List<Edge> kruskal() {
+        DSet dset = new DSet(N);
+        List<Edge> all = getEdges(true);
+        List<Edge> used = new ArrayList<>();
+        for (int i=0; i<all.size(); i++) {
+            Edge e = all.get(i);
+            if (dset.find(e.source) != dset.find(e.target)) {
+                dset.union(e.source, e.target);
+                used.add(e);
+            }
+        }
+        return used;
+    }
+}
