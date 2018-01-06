513c05c075e1b7a85b498881c4b6f5d9e4536303
sergey-melnychuk <serg.meln@gmail.com>
2018-01-07 00:20:26 +0100

    Add proper name for PQueueTest

--- java-algs/src/test/java/edu/algs/pqueue/PQueueTest.java
+++ java-algs/src/test/java/edu/algs/pqueue/PQueueTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class IntPQueueTest {
+public class PQueueTest {
 
     @Test
     void ordering() {
