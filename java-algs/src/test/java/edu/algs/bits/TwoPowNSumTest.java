6ce3515edd78410b87b306ef2fe4cecdb1281513
sergey-melnychuk <serg.meln@gmail.com>
2018-01-10 12:20:10 +0100

    Add custom data structure to represent binary sum of 2^n numbers

---
+++ java-algs/src/test/java/edu/algs/bits/TwoPowNSumTest.java
@@ -0,0 +1,69 @@
+package edu.algs.bits;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwoPowNSumTest {
+
+    @Test
+    void bits() {
+        //                           -9-76---2--
+        int x = Integer.parseInt("01011000100", 2);
+        List<Integer> expected = Arrays.asList(2, 6, 7, 9);
+        final List<Integer> actual = new ArrayList<>();
+        TwoPowNSum.bits(x, actual::add);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void empty() {
+        TwoPowNSum tpns = new TwoPowNSum();
+        assertEquals("", tpns.toString());
+    }
+
+    @Test
+    void count4() {
+        TwoPowNSum tpns = new TwoPowNSum();
+        tpns.add2pown(0);
+        assertEquals("1", tpns.toString());
+        tpns.add2pown(0);
+        assertEquals("10", tpns.toString());
+        tpns.add2pown(0);
+        assertEquals("11", tpns.toString());
+        tpns.add2pown(0);
+        assertEquals("100", tpns.toString());
+    }
+
+    @Test
+    void count1024() {
+        TwoPowNSum tpns = new TwoPowNSum();
+        for (int i=0; i<1024; i++) {
+            tpns.add2pown(0);
+            assertEquals(Integer.toBinaryString(i+1), tpns.toString());
+        }
+    }
+
+    @Test
+    void kcount1024() {
+        TwoPowNSum tpns = new TwoPowNSum();
+        for (long i=0; i<1024; i++) {
+            tpns.add2pownk(10, 10);
+            assertEquals(Long.toBinaryString(10*(i+1)*1024), tpns.toString());
+        }
+    }
+
+    @Test
+    void sum() {
+        TwoPowNSum tpns = new TwoPowNSum();
+        tpns.add2pownk(1, 4);
+        tpns.add2pownk(0, 4);
+        tpns.add2pownk(2, 6);
+        tpns.add2pownk(3, 4);
+        assertEquals(Integer.toBinaryString(68), tpns.toString()    );
+    }
+}
