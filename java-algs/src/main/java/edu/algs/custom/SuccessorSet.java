a7685e648bcd8ff9521f090f11089c699797635e
sergey-melnychuk <serg.meln@gmail.com>
2018-01-28 16:56:08 +0100

    [custom] successor set

---
+++ java-algs/src/main/java/edu/algs/custom/SuccessorSet.java
@@ -0,0 +1,73 @@
+package edu.algs.custom;
+
+/*
+
+Set of initially 0..n-1 integers with following features:
+- remove(x) removes element from a set
+- next(x) finds successor y of x that is minimum item y in the set that y >= x
+
+next(x) returns -1 if no successor is available in the set
+
+ */
+public interface SuccessorSet {
+    void remove(int x);
+    int next(int x);
+}
+
+// Modified disjoint set (union-find)
+class SuccessorSetImpl implements SuccessorSet {
+    private final int n;
+    private final int id[];
+    private final int val[];
+    private final int rank[];
+
+    private SuccessorSetImpl(int n) {
+        if (n <= 0) throw new IllegalArgumentException("n must be > 0");
+        this.n = n;
+        this.id = new int[n];
+        this.val = new int[n];
+        this.rank = new int[n];
+        for (int i=0; i<n; i++) {
+            id[i] = i;
+            val[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    private int root(int x) {
+        while (id[x] != x) x = id[x];
+        return x;
+    }
+
+    // O(log(N)) worst case
+    @Override
+    public void remove(int x) {
+        if (x < 0 || x >= n) throw new IllegalArgumentException("x must be >= 0 and < n");
+        int xroot = root(x);
+        if (x == n-1) {
+            // Specific case for largest item in the set, as it doesn't have a successor (by problem definition)
+            val[xroot] = -1;
+        } else {
+            int y = x + 1; // the successor's id
+            int yroot = root(y);
+
+            if (rank[yroot] >= rank[xroot]) {
+                id[xroot] = yroot;
+                rank[yroot] += rank[xroot];
+            } else {
+                id[yroot] = xroot;
+                rank[xroot] += rank[yroot];
+                val[xroot] = val[yroot]; // update root's value for new sub-set's root
+            }
+        }
+    }
+
+    // O(log(N)) worst case
+    @Override
+    public int next(int x) {
+        if (x < 0 || x >= n) throw new IllegalArgumentException("x must be >= 0 and < n");
+        return val[root(x)];
+    }
+
+    public static SuccessorSet create(int n) { return new SuccessorSetImpl(n); }
+}
